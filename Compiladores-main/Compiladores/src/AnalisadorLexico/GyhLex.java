a5acbcb30e52153ba3ebe2536175536878e0be3c
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2022-03-19 20:32:39 -0300

    Update GyhLex.java

--- Compiladores-main/Compiladores/src/AnalisadorLexico/GyhLex.java
+++ Compiladores-main/Compiladores/src/AnalisadorLexico/GyhLex.java
@@ -10,7 +10,7 @@ public class GyhLex {
 	public int cont_linhas = 0;
 	public LeitorArquivo ldat;
 	
-	public GyhLex(String args) {
+	public GyhLex() {
 		//constructor
 	}
 	
@@ -20,45 +20,44 @@ public class GyhLex {
 		String lexema = "";
 		String flexoes = "";
 		
-		while(!arraylinhas.isEmpty()) {
+		for(int j = 0; j < this.arraylinhas.size(); j++) {
+			System.out.println(this.arraytoken);
 			cont_linhas++;
-			flexoes = arraylinhas.get(0);//take first line of file
-			for(int i = 0; (i < flexoes.length()-1); i++) {//Start for
-				try {//try catch
-					if(flexoes.charAt(i+1) == '=') {//if
-						if(flexoes.charAt(i) == '=') {
-							arraytoken.add(new Token(TipoToken.OpRelIgual, "==", this.cont_linhas));
-						}else if(flexoes.charAt(i) == ':') {
-							arraytoken.add(new Token(TipoToken.Atrib, ":=", this.cont_linhas));
-						}else if(flexoes.charAt(i) == '!') {
-							arraytoken.add(new Token(TipoToken.OpRelDif, "!=", this.cont_linhas));
-						}else if(flexoes.charAt(i) == '>') {
-							arraytoken.add(new Token(TipoToken.OpRelMaiorIgual, ">=", this.cont_linhas));
-						}else if(flexoes.charAt(i) == '<') {
-							arraytoken.add(new Token(TipoToken.OpRelMenorIgual, "<=", this.cont_linhas));
-						}
-					}//end if
-				}catch(Exception e) {
-					 System.out.println(e);
-				}
-				if(flexoes.charAt(i) == '<') {
-					arraytoken.add(new Token(TipoToken.OpRelMenor, "<", this.cont_linhas));
+			flexoes = this.arraylinhas.get(j);//take first line of file
+			for(int i = 0; i < flexoes.length(); i++) {//Start for
+				if(i != flexoes.length() - 1 && flexoes.charAt(i+1) == '=') {//if
+					if(flexoes.charAt(i) == '=') {
+						this.arraytoken.add(new Token(TipoToken.OpRelIgual, "==", this.cont_linhas));
+					}else if(flexoes.charAt(i) == ':') {
+						this.arraytoken.add(new Token(TipoToken.Atrib, ":=", this.cont_linhas));
+					}else if(flexoes.charAt(i) == '!') {
+						this.arraytoken.add(new Token(TipoToken.OpRelDif, "!=", this.cont_linhas));
+					}else if(flexoes.charAt(i) == '>') {
+						this.arraytoken.add(new Token(TipoToken.OpRelMaiorIgual, ">=", this.cont_linhas));
+					}else if(flexoes.charAt(i) == '<') {
+						this.arraytoken.add(new Token(TipoToken.OpRelMenorIgual, "<=", this.cont_linhas));
+					}
+				}//end if
+				else if(flexoes.charAt(i) == '<') {
+					this.arraytoken.add(new Token(TipoToken.OpRelMenor, "<", this.cont_linhas));
 				}else if(flexoes.charAt(i) == '>') {
-					arraytoken.add(new Token(TipoToken.OpRelMaior, ">", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.OpRelMaior, ">", this.cont_linhas));
 				}else if(flexoes.charAt(i) == '*') {
-					arraytoken.add(new Token(TipoToken.OpAritMult, "*", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.OpAritMult, "*", this.cont_linhas));
 				}else if(flexoes.charAt(i) == '/') {
-					arraytoken.add(new Token(TipoToken.OpAritDiv, "/", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.OpAritDiv, "/", this.cont_linhas));
 				}else if(flexoes.charAt(i) == '+') {
-					arraytoken.add(new Token(TipoToken.OpAritSoma, "+", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.OpAritSoma, "+", this.cont_linhas));
 				}else if(flexoes.charAt(i) == '-') {
-					arraytoken.add(new Token(TipoToken.OpAritSub, "-", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.OpAritSub, "-", this.cont_linhas));
 				}else if(flexoes.charAt(i) == ':') {
-					arraytoken.add(new Token(TipoToken.Delim, ":", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.Delim, ":", this.cont_linhas));
 				}else if(flexoes.charAt(i) == '(') {
-					arraytoken.add(new Token(TipoToken.AbrePar, "(", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.AbrePar, "(", this.cont_linhas));
 				}else if(flexoes.charAt(i) == ')') {
-					arraytoken.add(new Token(TipoToken.FechaPar, ")", this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.FechaPar, ")", this.cont_linhas));
+				}else if(flexoes.charAt(i) == '#') { //fiz esse agora para o coment
+					continue;
 				}else if(flexoes.charAt(i) >= 48 && flexoes.charAt(i) <= 57) {//numbers
 					int contadorPontos = 0;
 					while(i < flexoes.length()) {
@@ -74,15 +73,15 @@ public class GyhLex {
 								System.out.println("erro mane");
 								System.exit(1);
 							}
-						}
+						}else break;
 						i--;
 						
 					}//while()
 					if(lexema.contains(".")) {//real numbers
-						arraytoken.add(new Token(TipoToken.NumReal, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.NumReal, lexema, this.cont_linhas));
 						lexema = "";
 					}else {//integers numbers
-						arraytoken.add(new Token(TipoToken.NumInt, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.NumInt, lexema, this.cont_linhas));
 						lexema = "";
 					}
 				}//end if numbers
@@ -93,7 +92,7 @@ public class GyhLex {
 							i++;
 						}
 					}
-					arraytoken.add(new Token(TipoToken.Cadeia, lexema, this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.Cadeia, lexema, this.cont_linhas));
 					lexema = "";
 					//end chain
 				}else if(flexoes.charAt(i) >= 97 && flexoes.charAt(i) >= 122) {//variables 
@@ -104,11 +103,11 @@ public class GyhLex {
 							lexema = lexema.concat(Character.toString(flexoes.charAt(i)));
 						}else if(flexoes.charAt(i) >= 48 && flexoes.charAt(i) <= 57) {
 							lexema = lexema.concat(Character.toString(flexoes.charAt(i)));
-						}
+						}else break;
 						i++;
 					}//end while
 					i--;
-					arraytoken.add(new Token(TipoToken.Var, lexema, this.cont_linhas));
+					this.arraytoken.add(new Token(TipoToken.Var, lexema, this.cont_linhas));
 					lexema = "";
 				}//end variables
 				else if(flexoes.charAt(i) >= 65 && flexoes.charAt(i) >= 90) {//reserved words
@@ -118,41 +117,43 @@ public class GyhLex {
 						if(flexoes.charAt(i) >= 65 && flexoes.charAt(i) >= 90) {
 							lexema = lexema.concat(Character.toString(flexoes.charAt(i)));
 							i++;
-						}
+							System.out.println(lexema);
+						}else break;
 					}//while
 					i--;
 					if(lexema.equals("E")) {
-						arraytoken.add(new Token(TipoToken.OpBoolE, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.OpBoolE, lexema, this.cont_linhas));
 					}else if(lexema.equals("OU")) {
-						arraytoken.add(new Token(TipoToken.OpBoolOu, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.OpBoolOu, lexema, this.cont_linhas));
 					}else if(lexema.equals("DEC")) {
-						arraytoken.add(new Token(TipoToken.PCDec, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCDec, lexema, this.cont_linhas));
 					}else if(lexema.equals("PROG")) {
-						arraytoken.add(new Token(TipoToken.PCProg, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCProg, lexema, this.cont_linhas));
 					}else if(lexema.equals("INT")) {
-						arraytoken.add(new Token(TipoToken.PCInt, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCInt, lexema, this.cont_linhas));
 					}else if(lexema.equals("LER")) {
-						arraytoken.add(new Token(TipoToken.PCLer, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCLer, lexema, this.cont_linhas));
 					}else if(lexema.equals("REAL")) {
-						arraytoken.add(new Token(TipoToken.PCReal, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCReal, lexema, this.cont_linhas));
 					}else if(lexema.equals("IMPRIMIR")) {
-						arraytoken.add(new Token(TipoToken.PCImprimir, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCImprimir, lexema, this.cont_linhas));
 					}else if(lexema.equals("SE")){
-						arraytoken.add(new Token(TipoToken.PCSe, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCSe, lexema, this.cont_linhas));
 					}else if(lexema.equals("ENTAO")) {
-						arraytoken.add(new Token(TipoToken.PCEntao, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCEntao, lexema, this.cont_linhas));
 					}else if(lexema.equals("ENQTO")) {
-						arraytoken.add(new Token(TipoToken.PCEnqto, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCEnqto, lexema, this.cont_linhas));
 					}else if(lexema.equals("INI")) {
-						arraytoken.add(new Token(TipoToken.PCIni, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCIni, lexema, this.cont_linhas));
 					}else if(lexema.equals("FIM")) {
-						arraytoken.add(new Token(TipoToken.PCFim, lexema, this.cont_linhas));
+						this.arraytoken.add(new Token(TipoToken.PCFim, lexema, this.cont_linhas));
 					}
 					lexema = "";
 				}//end reserved words
 				
 			}//end for
-		}
+		}//end forzão
+		System.out.println("oi Andsu");
 	}		
 }
 
